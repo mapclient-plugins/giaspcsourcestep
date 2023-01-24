68805c96f04244c2f9b45c77f4cb205dfa0fb2d9
TimmyNic <tim.nicolas@outlook.com>
2023-01-24 13:55:55 +1300

    Upgrade to PySide6.

--- mapclientplugins/giaspcsourcestep/configuredialog.py
+++ mapclientplugins/giaspcsourcestep/configuredialog.py
@@ -1,5 +1,5 @@
 import os
-from PySide2 import QtWidgets
+from PySide6 import QtWidgets
 from mapclientplugins.giaspcsourcestep.ui_configuredialog import Ui_Dialog
 
 INVALID_STYLE_SHEET = 'background-color: rgba(239, 0, 0, 50)'
--- mapclientplugins/giaspcsourcestep/step.py
+++ mapclientplugins/giaspcsourcestep/step.py
@@ -4,7 +4,7 @@ MAP Client Plugin Step
 import os.path
 import json
 
-from PySide2 import QtGui, QtWidgets
+from PySide6 import QtGui
 
 from mapclient.mountpoints.workflowstep import WorkflowStepMountPoint
 from mapclientplugins.giaspcsourcestep.configuredialog import ConfigureDialog
